5a89e1098b08c4fc5c84886c982e73f715fd1e65
Keenan Deyce <keenandeyce@gmail.com>
2023-11-16 17:51:11 +0200

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -99,5 +99,10 @@ router.post("/profile/avatar", upload.single('avatar'), uploadAvatar)
 router.get("/avatar/:userId", displayAvatar)
 // google auth
 router.get("/auth/google", googleAuth)
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/dashboard-b');
+});
 
 module.exports = router;
