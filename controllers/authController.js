510a6b0e50befe833bb2855c23ec5668615e6281
Keenan Deyce <keenandeyce@gmail.com>
2023-11-22 14:33:41 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -142,7 +142,7 @@ const loginUser = async (req, res) => {
                 {expiresIn: "1h"}, 
                 (err, token) => {
                     if (err) throw err;
-                    res.cookie("authToken", token, { sameSite: "none", secure: true }).json(user)
+                    res.cookie("authToken", token, { sameSite: "none", secure: true }).json(user, token);
                 }
             )  
         } 
