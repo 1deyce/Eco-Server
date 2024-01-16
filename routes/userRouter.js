61fc5c06495c26229fd5ebed445f875be590a737
Keenan Deyce <keenandeyce@gmail.com>
2024-01-16 10:16:57 +0200

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -42,6 +42,6 @@ router.post("/profile/avatar", uploadAvatar)
 router.get("/avatar/:userId", displayAvatar)
 router.put("/addresses/:id", updateAddress)
 // feedback form
-router.get("/feedback", submitFeedback)
+router.post("/feedback", submitFeedback)
 
 module.exports = router;
