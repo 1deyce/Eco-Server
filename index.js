8068a70369fc179731123b77269fc2cf614d8a29
1deyce <124916049+1deyce@users.noreply.github.com>
2024-09-20 15:41:23 +0200

    Updated CORS for vercel?

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(cors({
   origin: process.env.PROD_URL,
   // origin: "http://localhost:3000",
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  methods: ['GET','OPTIONS', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
