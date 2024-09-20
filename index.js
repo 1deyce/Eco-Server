4a58d17704f74748365f3596e70a6c1a5460313f
1deyce <124916049+1deyce@users.noreply.github.com>
2024-09-20 16:17:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,16 +18,37 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false})) 
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', 'https://ecotrack-solutions.netlify.app')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+const corsHandler = allowCors(handler);
+
 app.use(cors({
   origin: process.env.PROD_URL,
   // origin: "http://localhost:3000",
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-  optionsSuccessStatus: 200,
 }));
 
-app.use("/", require("./routes/userRouter"))
+app.use("/", require("./routes/userRouter"), corsHandler);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running on ${port}`));
