3091a0c20ebd56227f3b9f64901344c3b2b09661
1deyce <124916049+1deyce@users.noreply.github.com>
2024-09-20 16:06:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
+  optionsSuccessStatus: 200,
 }));
 
 app.use("/", require("./routes/userRouter"))
